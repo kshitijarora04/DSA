27d9a665f92377988d5f4be010f647a1d5cff794
Kshitij Arora <kshitijarora04@gmail.com>
2025-07-28 19:23:35 +0530

    all operations on singly linked list

--- singlyLL.cpp
+++ singlyLL.cpp
@@ -54,21 +54,110 @@ public:
     }
 
     // insert at the end of the linked list
+    // pointer is of the type to the type which it points to it
     void insert_at_end(int val)
     {
-        node *temp2 = head;
         node *temp = new node();
         temp->data = val;
-        while (temp2->next != NULL)
+        if (head == NULL)
         {
-            temp2 = temp2->next;
+            head = temp;
         }
-        temp2->next = temp;
+        else
+        {
+            node *t = head;
+            while (t->next != NULL)
+            {
+                t = t->next;
+            }
+            t->next = temp;
+        }
     }
 
     // insert in the middle
-    void insert_in_middle(int pos, int val)
+    void insert_in_middle(int val, int pos)
     {
+        node *temp = new node();
+        temp->data = val;
+
+        if (pos == 1)
+        {
+            temp->next = head;
+            head = temp;
+        }
+        else
+        {
+            node *t = head;
+            while (((pos - 2) != 0) && (t->next != NULL))
+            {
+                t = t->next;
+                pos--;
+            }
+            temp->next = t->next;
+            t->next = temp;
+        }
+    }
+
+    void delete_at_beginning()
+    {
+        if (head == NULL)
+        {
+            cout << "Deletion not possible" << endl;
+        }
+        else
+        {
+            node *temp = head;
+            head = head->next;
+            delete (temp);
+        }
+    }
+
+    void delete_at_end()
+    {
+        if (head == NULL)
+        {
+            cout << "List is Empty" << endl;
+        }
+        else if (head->next == NULL)
+        {
+            delete (head);
+            head = NULL;
+        }
+        else
+        {
+            node *t = head;
+            while (t->next->next != NULL)
+            {
+                t = t->next;
+            }
+            delete (t->next);
+            t->next = NULL;
+        }
+    }
+    void delete_at_pos(int pos)
+    {
+        if (head == NULL)
+        {
+            cout << "Not Possible" << endl;
+        }
+        else if (pos == 1)
+        {
+            node *t = head;
+            head = head->next;
+            delete (t);
+        }
+        else
+        {
+            node *t = head;
+            while (((pos - 2) != 0) && (t->next != NULL))
+            {
+                t = t->next;
+                pos--;
+            }
+            node *temp = t->next;
+            t->next = temp->next;
+            delete (temp);
+        }
     }
 };
 
@@ -80,6 +169,20 @@ int main()
     ll.insert_at_beg(30);
     ll.insert_at_beg(40);
     ll.insert_at_end(50);
+    ll.insert_in_middle(100, 4);
+    cout << "After Insertion" << endl;
+    ll.print();
+    cout << endl;
+    ll.delete_at_beginning();
+    cout << "After deleting from beginning" << endl;
+    ll.print();
+    cout << endl;
+    ll.delete_at_end();
+    cout << "After deleting from end" << endl;
+    ll.print();
+    cout << endl;
+    ll.delete_at_pos(3);
+    cout << "After deleting from position 3" << endl;
     ll.print();
     return 0;
 }
